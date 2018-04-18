fd39d48bce7117ce648ea95a0e745698ba44e8f5
MaciekTalaska <maciek.talaska@gmail.com>
2018-04-18 11:42:19 +0200

    processing diceware files

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,38 @@
-
 pub mod mrandom;
-//use mrandom;
-//use std::collections::HashMap;
 
-//const diceware_files: HashMap<&str, &str> =
-//[("en", "diceware-en.txt"),
-// ("pl", "diceware-pl.txt")];
-// //.iter().cloned().collect();
+struct DicewareInfo {
+    language:   String,
+    num_dices:  u8,
+    words:      Vec<String>
+}
 
-fn process_diceware_words(message: &str) {
-    let s4 =  message
+fn calculate_max_dice_count(size: usize) -> u8 {
+    let fsize: f32 = size as f32;
+    let result = fsize.log(6.0).abs().ceil() as u8;
+    return result;
+}
+
+fn process_diceware_words(message: &str) -> DicewareInfo {
+    let words =  message
         .lines()
         .map(|l| l.split_whitespace().last().unwrap())
-        .collect::<Vec<&str>>();
+        .map( |s| s.to_string())
+        .collect::<Vec<String>>();
 
-    println!("sp4: {:?}", s4);
+    println!("first word: {:?}", words[0]);
+    println!("second word:  {:?}", words[1]);
+
+    return DicewareInfo{language: "en".to_string(),
+        num_dices: calculate_max_dice_count(words.len()),
+        words};
 }
 
-fn read_diceware_list() {
-    let bytes = include_bytes!("diceware-en.txt");
-    let words = String::from_utf8_lossy(bytes);
-    process_diceware_words(&words);
-}
-
-
-//fn show_files() {
-//    for (k, v) in DICEWARE_FILES {
-//        println!("key: {:?} value: {:?}", k, v);
-//    }
-//}
-
-fn main() {
-    mrandom::dice_roll_test();
-    mrandom::dice_rolls_test();
-    //read_diceware_list();
-//    show_files();
+pub fn read_diceware_list() {
+    let bytes = include_bytes!("diceware-pl.txt");
+    let words = String::from_utf8_lossy(bytes).to_string().to_owned();
+    let info = process_diceware_words(&words);
+    println!("language: {:?}", info.language);
+    println!("num_dices: {:?}", info.num_dices);
+    println!("words[0]: {:?}", info.words[0]);
+    println!("words.length: {:?}", info.words.len());
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ extern crate dpg;
 fn main() {
     println!("hello world from main of dpg!");
 
-    dpg::mrandom::dice_roll_test();
-    dpg::mrandom::dice_rolls_test();
-    //read_diceware_list();
+    //dpg::mrandom::dice_roll_test();
+    //dpg::mrandom::dice_rolls_test();
+    dpg::read_diceware_list();
 //    show_files();
 }
