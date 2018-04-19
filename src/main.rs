89ddfbff15da3c1d4b648b6683fac34e629223a1
MaciekTalaska <maciek.talaska@gmail.com>
2018-04-19 08:57:55 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,14 @@ fn get_diceware_info_by_language(language: &str, diceware_data: &Vec<DicewareInf
 
 fn get_random_word(language: &str, diceware_data: &Vec<DicewareInfo>) -> String {
     let info: DicewareInfo = get_diceware_info_by_language(language, diceware_data.as_ref());
-//    let info = match language.to_lowercase().as_str() {
-//        "pl" => &diceware_data[1],
-//        _ => &diceware_data[0]
-//    };
-    println!("number of dice rolls: {:?}", info.num_dices);
+    //println!("number of dice rolls: {:?}", info.num_dices);
     let result = dpg::mrandom::roll_dices(info.num_dices);
 
-    println!("index: {:?}", result);
+    //println!("index: {:?}", result);
     info.words[result as usize % info.words.len()].clone()
 }
 
 fn main() {
-    //dpg::mrandom::dice_roll_test();
-    //dpg::mrandom::dice_rolls_test();
     let all_diceware = dpg::read_all_diceware_lists();
-//    for info in all_diceware {
-//        dpg::print_diceware_info(info);
-//    }
     println!("random word: {:?}", get_random_word("pl",&all_diceware));
 }
--- src/mrandom/mod.rs
+++ src/mrandom/mod.rs
@@ -15,21 +15,6 @@ pub fn roll_dice_test() {
 }
 
 pub fn roll_dices(dices: u8) -> u32 {
-//    println!("dices to throw: {:?}", dices);
-//    for i in {0..dices} {
-//        println!("current dice throw: {:?} result: {:?}", i, dice_roll());
-//    }
-//    return dices as usize;
-
-//    let mut all_dices= vec![0; dices as usize];
-//    for i in {0..dices} {
-//        all_dices[i as usize] = dice_roll() as u8;
-//    }
-//    let mut all_dices = Vec::<u8>::new();
-//    for i in {0..dices} {
-//        all_dices.push(roll_dice());
-//    }
-//    return all_dices as Vec<u8>;
     let mut index: u32 = 0;
     for i in {0..dices} {
         if (index == 0) {
