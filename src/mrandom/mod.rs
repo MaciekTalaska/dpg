e75a05eb8139eb5baf8e9c582f23b4dd1202ce33
MaciekTalaska <maciek.talaska@gmail.com>
2018-04-19 20:45:50 +0200

    roll_dices() algorithm simplified

--- src/mrandom/mod.rs
+++ src/mrandom/mod.rs
@@ -7,24 +7,11 @@ pub fn roll_dice() -> u8 {
     return r % 6 + 1;
 }
 
-pub fn roll_dice_test() {
-    let index = roll_dice();
-    println!("dice roll returned: {:?}", index);
-}
-
 pub fn roll_dices(dices: u8) -> u32 {
     let mut index: u32 = 0;
     for _i in {0..dices} {
-        if index == 0 {
-            index += roll_dice() as u32;
-        } else {
-            index += roll_dice() as u32;
-            index *= 6;
-        }
+        index *= 6;
+        index += roll_dice() as u32;
     }
     return index;
-}
-
-pub fn roll_dices_test() {
-    roll_dices(5);
 }
