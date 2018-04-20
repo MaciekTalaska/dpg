c26349dbe26360645348116e26fbc4140b27295f
MaciekTalaska <maciek.talaska@gmail.com>
2018-04-20 12:59:10 +0200

    minor changes to tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -7,6 +7,10 @@ fn classic_dice_roll_should_always_return_value_from_1_to_6() {
     assert!(result <= 6);
 }
 
+fn max_by_dice_num(dice_num: u8) -> u32 {
+    (6 as u32).pow(dice_num as u32) -1
+}
+
 #[test]
 fn roll_dices_1_dice_should_return_value_from_0_to_5() {
     let result = dpg::mrandom::roll_dices(1);
@@ -16,23 +20,23 @@ fn roll_dices_1_dice_should_return_value_from_0_to_5() {
 #[test]
 fn roll_dices_2_dices_should_not_exceed_35() {
     let result = dpg::mrandom::roll_dices(2);
-    assert!(result <= 35);
+    assert!(result <= max_by_dice_num(2));
 }
 
 #[test]
-fn roll_dices_3_dices_should_not_excedeed_215() {
+fn roll_dices_3_dices_should_not_exceed_215() {
     let result = dpg::mrandom::roll_dices(3);
-    assert!(result <= 215);
+    assert!(result <= max_by_dice_num(3));
 }
 
 #[test]
 fn roll_dices_4_dices_should_not_exceed_1295() {
     let result = dpg::mrandom::roll_dices(4);
-    assert!(result <= 1295);
+    assert!(result <= max_by_dice_num(4));
 }
 
 #[test]
 fn roll_dices_5_dices_should_not_exceed_7775() {
     let result = dpg::mrandom::roll_dices(5);
-    assert!(result <= 7775);
+    assert!(result <= max_by_dice_num(5));
 }
