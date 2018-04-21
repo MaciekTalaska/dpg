9fbcb739fa94f5b2a4706fa7cb0862a6916f0d62
MaciekTalaska <maciek.talaska@gmail.com>
2018-04-21 15:10:34 +0200

    run some tests multiple times

--- tests/tests.rs
+++ tests/tests.rs
@@ -19,24 +19,32 @@ fn roll_dices_1_dice_should_return_value_from_0_to_5() {
 
 #[test]
 fn roll_dices_2_dices_should_not_exceed_35() {
-    let result = dpg::mrandom::roll_dices(2);
-    assert!(result <= max_by_dice_num(2));
+    for _i in {0..10} {
+        let result = dpg::mrandom::roll_dices(2);
+        assert!(result <= max_by_dice_num(2));
+    }
 }
 
 #[test]
 fn roll_dices_3_dices_should_not_exceed_215() {
-    let result = dpg::mrandom::roll_dices(3);
-    assert!(result <= max_by_dice_num(3));
+    for _i in {0..40} {
+        let result = dpg::mrandom::roll_dices(3);
+        assert!(result <= max_by_dice_num(3));
+    }
 }
 
 #[test]
 fn roll_dices_4_dices_should_not_exceed_1295() {
-    let result = dpg::mrandom::roll_dices(4);
-    assert!(result <= max_by_dice_num(4));
+    for _i in {0..100} {
+        let result = dpg::mrandom::roll_dices(4);
+        assert!(result <= max_by_dice_num(4));
+    }
 }
 
 #[test]
 fn roll_dices_5_dices_should_not_exceed_7775() {
-    let result = dpg::mrandom::roll_dices(5);
-    assert!(result <= max_by_dice_num(5));
+    for _i in {0..1000} {
+        let result = dpg::mrandom::roll_dices(5);
+        assert!(result <= max_by_dice_num(5));
+    }
 }
