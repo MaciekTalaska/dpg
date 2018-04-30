967ec8756cb03e3d43fe462a2bde2e5662d843fa
MaciekTalaska <maciek.talaska@gmail.com>
2018-04-30 10:15:43 +0200

    using OsRng - crypto secure rnd

--- src/mrandom/mod.rs
+++ src/mrandom/mod.rs
@@ -1,17 +1,33 @@
 extern crate rand;
 
+use mrandom::rand::Rng;
+
 pub mod mrandom;
 
+fn get_rnd() -> rand::OsRng {
+    let rng = match rand::OsRng::new() {
+        Ok(result) => result,
+        Err(error) => panic!("Unable to obtain crypto secure random number generator! {}", error)
+    };
+    return rng;
+}
+
 pub fn roll_dice() -> u8 {
-    let r = rand::random::<u8>();
-    return r % 6 + 1;
+    let mut rng = get_rnd();
+    return roll_dice_internal(&mut rng);
+}
+
+fn roll_dice_internal(rng: &mut rand::OsRng) -> u8 {
+    let value = rng.gen::<u8>();
+    return value % 6 + 1;
 }
 
 pub fn roll_dices(dices: u8) -> u32 {
     let mut index: u32 = 0;
+    let mut rng = get_rnd();
     for _i in {0..dices} {
         index *= 6;
-        index += (roll_dice() as u32) -1;
+        index += (roll_dice_internal(&mut rng) as u32) -1;
     }
     return index;
 }
