ee46eb62e8940753da84f84a580a815c3dfa4f47
MaciekTalaska <maciek.talaska@gmail.com>
2018-05-25 21:30:55 +0200

    extracted code to lib: preparing for top down testing

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,17 @@ pub mod dices;
 pub mod option_parser;
 pub mod diceware_info;
 pub mod passwords;
+
+
+// this function was extracted only to make it possible to test
+// the whole solution
+pub fn generate_diceware_passwords(args: Vec<String>) -> String {
+    let options = self::option_parser::parse_command_line(args);
+
+    #[cfg(debug_assertions)]
+        println!("Options: {:?}", options);
+
+    let diceware_repository = self::diceware_info::read_all_diceware_lists();
+
+    passwords::generate_diceware_passwords(&options, diceware_repository)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
 use std::env;
+extern crate dpg;
 
 pub mod dices;
 pub mod option_parser;
 pub mod diceware_info;
 pub mod passwords;
 
-
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let options = self::option_parser::parse_command_line(args);
 
-    #[cfg(debug_assertions)]
-        println!("Options: {:?}", options);
-
-    let diceware_repository = self::diceware_info::read_all_diceware_lists();
-
-    let passwords = passwords::generate_diceware_passwords(&options, diceware_repository);
+    let passwords = dpg::generate_diceware_passwords(args);
 
     // this should be printed probably only with "-verbose"
     println!("generated password(s):\n{}", passwords);
 }
+
