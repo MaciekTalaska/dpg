8925843846450060a27b074d03680f5849caee47
MaciekTalaska <maciek.talaska@gmail.com>
2018-05-27 22:27:00 +0200

    tests refactoring

--- src/dices.rs
+++ src/dices.rs
@@ -29,3 +29,59 @@ pub fn roll_dices(dices: u8) -> u32 {
     }
     return index;
 }
+
+
+#[cfg(test)]
+mod dices_tests {
+    use dices;
+
+    #[test]
+    fn classic_dice_roll_should_always_return_value_from_1_to_6() {
+        let result = dices::roll_dice();
+        assert!(result >= 1);
+        assert!(result <= 6);
+    }
+
+    fn max_by_dice_num(dice_num: u8) -> u32 {
+        (6 as u32).pow(dice_num as u32) -1
+    }
+
+    #[test]
+    fn roll_dices_1_dice_should_return_value_from_0_to_5() {
+        let result = dices::roll_dices(1);
+        assert!(result <= max_by_dice_num(1));
+    }
+
+    #[test]
+    fn roll_dices_2_dices_should_not_exceed_35() {
+        for _i in {0..10} {
+            let result = dices::roll_dices(2);
+            assert!(result <= max_by_dice_num(2));
+        }
+    }
+
+    #[test]
+    fn roll_dices_3_dices_should_not_exceed_215() {
+        for _i in {0..40} {
+            let result = dices::roll_dices(3);
+            assert!(result <= max_by_dice_num(3));
+        }
+    }
+
+    #[test]
+    fn roll_dices_4_dices_should_not_exceed_1295() {
+        for _i in {0..100} {
+            let result = dices::roll_dices(4);
+            assert!(result <= max_by_dice_num(4));
+        }
+    }
+
+    #[test]
+    fn roll_dices_5_dices_should_not_exceed_7775() {
+        for _i in {0..1000} {
+            let result = dices::roll_dices(5);
+            assert!(result <= max_by_dice_num(5));
+        }
+    }
+
+}
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,50 +1,9 @@
 extern crate dpg;
 
 #[test]
-fn classic_dice_roll_should_always_return_value_from_1_to_6() {
-    let result = dpg::dices::roll_dice();
-    assert!(result >= 1);
-    assert!(result <= 6);
-}
-
-fn max_by_dice_num(dice_num: u8) -> u32 {
-    (6 as u32).pow(dice_num as u32) -1
-}
-
-#[test]
-fn roll_dices_1_dice_should_return_value_from_0_to_5() {
-    let result = dpg::dices::roll_dices(1);
-    assert!(result <= max_by_dice_num(1));
-}
-
-#[test]
-fn roll_dices_2_dices_should_not_exceed_35() {
-    for _i in {0..10} {
-        let result = dpg::dices::roll_dices(2);
-        assert!(result <= max_by_dice_num(2));
-    }
-}
-
-#[test]
-fn roll_dices_3_dices_should_not_exceed_215() {
-    for _i in {0..40} {
-        let result = dpg::dices::roll_dices(3);
-        assert!(result <= max_by_dice_num(3));
-    }
-}
-
-#[test]
-fn roll_dices_4_dices_should_not_exceed_1295() {
-    for _i in {0..100} {
-        let result = dpg::dices::roll_dices(4);
-        assert!(result <= max_by_dice_num(4));
-    }
-}
-
-#[test]
-fn roll_dices_5_dices_should_not_exceed_7775() {
-    for _i in {0..1000} {
-        let result = dpg::dices::roll_dices(5);
-        assert!(result <= max_by_dice_num(5));
-    }
+fn generating_passwords() {
+    let args = vec![ String::from("-l:en"), String::from("-w:4"), String::from("-p:1")];
+    let passwords = dpg::generate_diceware_passwords(args);
+    assert!(passwords.len() > 0);
+    assert_eq!(passwords.lines().count(), 1);
 }
