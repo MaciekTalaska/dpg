554316cd9d4fe83cf1b8f2cd05d5c2d8737e4add
MaciekTalaska <maciek.talaska@gmail.com>
2018-08-03 09:27:04 +0200

    show additional info when in debug

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ extern crate dpg;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     let options = dpg::option_parser::parse_command_line(args);
 
-    //let passwords = dpg::generate_diceware_passwords(args);
+    #[cfg(debug_assertions)]
+    println!("Options: {:?}", options);
+
     let passwords = dpg::generate_diceware_passwords_new(options);
 
     // this should be printed probably only with "-verbose"
